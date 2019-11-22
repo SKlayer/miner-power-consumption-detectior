24294a3d4ddec7ae707ece73828e9cc29f793c42
hanlin.chen <hanlin.chen@bitmain.com>
2019-11-22 17:36:22 +0800

    fixbug: ip argv parse fail

--- power_server.py
+++ power_server.py
@@ -1,21 +1,23 @@
 import time
-from util import logger
-import init
-import globals
+import traceback
+from concurrent.futures import ThreadPoolExecutor
+
 import tornado.ioloop
-import tornado.web
 import tornado.options
+import tornado.web
 from tornado import gen
 from tornado.concurrent import run_on_executor
-from concurrent.futures import ThreadPoolExecutor
-import traceback
 
-
-
+import globals
+import init
+from util import logger
 
 
 class api_v1_power(tornado.web.RequestHandler):
-    executor = ThreadPoolExecutor(len(globals.COM_SHARD)*2)
+    if len(globals.COM_SHARD) <= 0:
+        executor = ThreadPoolExecutor(1)
+    else:
+        executor = ThreadPoolExecutor(len(globals.COM_SHARD) * 2)
 
     @run_on_executor
     def read_power_by_IP(self, ip):
@@ -29,35 +31,38 @@ class api_v1_power(tornado.web.RequestHandler):
     @gen.coroutine
     def get(self):
         start_point = time.time()
-        ipaddr = self.get_argument("ip")
-        e = None
-        if ipaddr is None:
+        if self.request.arguments.has_key("ip"):
+            ipaddr = self.get_argument("ip")
+        else:
             ipaddr = self.request.remote_ip
+
+        e = None
+
         if ipaddr not in globals.METERS_IP_MAP:
             elapsed = round(time.time() - start_point, 3)
 
             self.write({'time': time.time(),
-                            "version": "0.0",
-                            "power": -1,
-                            "error": True,
-                            "msg": "IP Not Found",
-                            "ipaddr": ipaddr,
-                            "elapsed": elapsed,
-                            })
+                        "version": "0.0",
+                        "power": -1,
+                        "error": True,
+                        "msg": "IP Not Found",
+                        "ipaddr": ipaddr,
+                        "elapsed": elapsed,
+                        })
         else:
             try:
                 power = yield self.read_power_by_IP(ipaddr)
                 elapsed = round(time.time() - start_point, 3)
                 self.write({
-                        'time': time.time(),
-                        "version": "0.0",
-                        "power": power,
-                        "error": False,
-                        "msg": "success",
-                        "ipaddr": ipaddr,
-                        "elapsed": elapsed,
-                    })
-                logger.debug("Request %s , cost %sms." % (ipaddr, round(elapsed*1000, 3)))
+                    'time': time.time(),
+                    "version": "0.0",
+                    "power": power,
+                    "error": False,
+                    "msg": "success",
+                    "ipaddr": ipaddr,
+                    "elapsed": elapsed,
+                })
+                logger.debug("Request %s , cost %sms." % (ipaddr, round(elapsed * 1000, 3)))
             except Exception as excepts:
                 traceback.print_exc()
                 e = excepts
@@ -66,20 +71,20 @@ class api_v1_power(tornado.web.RequestHandler):
             elapsed = round(time.time() - start_point, 3)
             logger.exception("Meters read error:%s" % traceback.format_exc())
             self.write({'time': time.time(),
-                            "version": "0.0",
-                            "power": -1,
-                            "error": True,
-                            "msg": e,
-                            "ipaddr": ipaddr,
-                            "elapsed": elapsed,
-                            })
+                        "version": "0.0",
+                        "power": -1,
+                        "error": True,
+                        "msg": e,
+                        "ipaddr": ipaddr,
+                        "elapsed": elapsed,
+                        })
         self.finish()
+
 
 class ra9(tornado.web.RequestHandler):
     def get(self):
         self.write("I am alive")
         self.finish()
-
 
 
 def make_app():
@@ -88,8 +93,9 @@ def make_app():
         (r"/ra9", ra9),
     ], autoreload=False)
 
+
 def run():
     app = make_app()
-    logger.info("Server listening on %s:%s" % ( init.LISTEN_IP, init.LISTEN_PORT,))
+    logger.info("Server listening on %s:%s" % (init.LISTEN_IP, init.LISTEN_PORT,))
     app.listen(init.LISTEN_PORT, address=init.LISTEN_IP)
     tornado.ioloop.IOLoop.current().start()
